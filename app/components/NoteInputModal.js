11989712644eaac62d7dd7b5bbb3a58cee4a53e6
Phạm Văn Dương <duongpham321dz@gmail.com>
2023-02-24 12:53:14 +0700

    add-font
    font text input

--- app/components/NoteInputModal.js
+++ app/components/NoteInputModal.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { View,Image, StyleSheet, Modal,Text,Dimensions, StatusBar, TextInput, TouchableWithoutFeedback,Keyboard, KeyboardAvoidingView, Alert, TouchableOpacity } from "react-native";
+import React, { useEffect, useState,useRef } from "react";
+import { View,Image, NativeModules,StyleSheet, Modal,Text,Dimensions, StatusBar, TextInput, TouchableWithoutFeedback,Keyboard, KeyboardAvoidingView, Alert, TouchableOpacity } from "react-native";
 import colors from "../misc/colors";
 import RoundIconBtn from "./RoundIconBtn";
 import * as ImagePicker from 'expo-image-picker';
@@ -11,7 +11,48 @@ const NoteInputModal = ({visible, onClose,onSubmit,note,isEdit,isFocus})=>{
     const [title,setTitle] = useState('')
     const [description,setDescription] = useState('')
     const [image,setImage] = useState(null)
+
+
+    const [fontFamily,setFontFamily] = useState('default') 
+    const [selectedText, setSelectedText] = useState('');
+    const [currentFontFamily, setCurrentFontFamily] = useState('default');
+    const inputRef = useRef(null);
+
+    const handleSelectionChange = ({ nativeEvent: { selection, text } }) => {
+        if (text) {
+          setSelectedText(text.substring(selection.start, selection.end));
+        } else {
+          setSelectedText('');
+        }
+      };
+
+      const handleFontChange = (newFontFamily) => {
+        setFontFamily(newFontFamily);
+        setCurrentFontFamily(newFontFamily);
     
+        // Modify the selected text
+        if (selectedText) {
+          const selection = UIManager.getSelectionLayout(
+            findNodeHandle(inputRef.current)
+          );
+          UIManager.updateView(
+            [
+              {
+                findNodeHandle: () => selection.start,
+                callback: (node) => {
+                  const newNode = document.createElement('span');
+                  newNode.style.fontFamily = newFontFamily;
+                  node.parentNode.insertBefore(newNode, node);
+                  newNode.appendChild(node);
+                },
+              },
+            ],
+            () => {},
+          );
+        }
+      };
+    
+
     const handleModalClose=()=>{
         Keyboard.dismiss();
     }
@@ -99,30 +140,44 @@ const NoteInputModal = ({visible, onClose,onSubmit,note,isEdit,isFocus})=>{
                         placeholder="Title" 
                         style={[styles.input,styles.title]}
                         autoFocus={isFocus}
-                        multiline={true}/>
+                        multiline={true}
+                        />
                         {image && (
                             <>
+
                             <View>
                                 <TouchableOpacity onPress={handleImagePicker}>
-                                <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+                                    <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
                                 </TouchableOpacity>
-                                
+
                                 <RoundIconBtn
                                 onPress={() => setImage(null)}
                                 size={15}
                                 antIconName={'delete'}
                                 style={{ position: 'absolute', top: 0, right: 0 }}
                                 />
+
                             </View>
                             </>
                         )}
+                    <View style={styles.fontContainer}>
+                        <TouchableOpacity onPress={() => handleFontChange('Arial')}>
+                        <Text style={styles.fontBtn}>Arial</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => handleFontChange('Roboto')}>
+                        <Text style={styles.fontBtn}>Roboto</Text>
+                        </TouchableOpacity>
+                    </View>
                     <TextInput
+                        ref={inputRef}
                         value={description}
                         onChangeText={(text)=>handleOnchangeText(text,'description')} 
                         placeholder="Note" 
                         style={[styles.input,styles.description]}
                         autoFocus={!isFocus}
-                        multiline={true}/>      
+                        multiline={true}
+                        onSelectionChange={handleSelectionChange}/>
+    
                 </View>
 
                 <TouchableWithoutFeedback onPress={handleModalClose}>
@@ -142,7 +197,6 @@ const styles = StyleSheet.create({
     input:{
         fontSize : 20,
         color: colors.Dark,
-
     },
     title:{
         // height: 30,
@@ -173,6 +227,11 @@ const styles = StyleSheet.create({
         width:'100%',
         // backgroundColor:'red'
     },
+    fontContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingVertical: 10,
+      },
 
 })
 
